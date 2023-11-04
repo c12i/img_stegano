a8fd7b7b1f23f1d40bdc84959f1a8fa9fd64d63a
Collins Muriuki <hello@collinsmuriuki.xyz>
2023-11-04 21:14:56 +0300

    Add decode and encode wasm bindings

--- img_stegano_wasm/src/lib.rs
+++ img_stegano_wasm/src/lib.rs
@@ -1,13 +1,14 @@
 mod utils;
 
+use img_stegano_rs::ImgStegano;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+pub fn encode_text(input_image: &[u8], message: &str) -> Vec<u8> {
+    ImgStegano::encode_from_u8_array(input_image, message).unwrap()
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, img-stegano-wasm!");
+pub fn decode_text(input_image: &[u8]) -> String {
+    ImgStegano::decode_from_u8_array(input_image).unwrap()
 }
