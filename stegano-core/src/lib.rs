ba56cecf1c0d52b4aca4a5726c54741ce13360b1
Collins Muriuki <hello@collinsmuriuki.xyz>
2023-11-08 12:27:09 +0300

    Try lsb replacement

--- stegano-core/src/lib.rs
+++ stegano-core/src/lib.rs
@@ -11,26 +11,31 @@ pub struct ImgStegano;
 impl ImgStegano {
     pub fn encode_from_image(Image(input_image): Image, message: &str) -> Image {
         let mut output_image = input_image.clone();
-        let message = message
-            .as_bytes()
-            .iter()
-            .fold(String::new(), |acc, &byte| acc + &format!("{:08b}", byte));
+        let message = message.as_bytes();
         let (width, height) = output_image.dimensions();
-        let mut message = message.chars();
+        let mut message_idx = 0;
 
         for y in 0..height {
             for x in 0..width {
                 let pixel = output_image.get_pixel(x, y);
                 let mut rgba = pixel.to_rgba();
-                let message_bit = message.next();
-                if let Some(bit) = message_bit {
-                    if let Some(lsb) = bit.to_digit(2) {
-                        let lsb = lsb as u8;
-                        rgba.0[0] = (rgba.0[0] & 0xFE) | lsb;
-                        output_image.put_pixel(x, y, Rgba(rgba.0));
+
+                for i in 0..3 {
+                    let channel_bit = (rgba.0[i]) as u8;
+                    let message_bit = (message[message_idx - 1] >> i) & 1;
+                    if channel_bit != message_bit {
+                        // flip the LSB to match the message bit
+                        rgba.0[i] ^= 1;
                     }
-                } else {
-                    break; // All message bits have been encoded
+                    if message_idx < message.len() {
+                        message_idx += 1;
+                    } else {
+                        break; // all message bits encoded
+                    }
+                }
+                output_image.put_pixel(x, y, Rgba(rgba.0));
+                if message_idx >= message.len() {
+                    break; // all message bits encoded
                 }
             }
         }
@@ -64,7 +69,7 @@ impl ImgStegano {
 
     pub fn decode_from_image(Image(encoded_image): &Image) -> String {
         let (width, height) = encoded_image.dimensions();
-        let mut binary_message = String::new();
+        let mut message = Vec::new();
         let mut current_byte = 0u8;
         let mut bit_count = 0;
 
@@ -72,23 +77,26 @@ impl ImgStegano {
             for x in 0..width {
                 let pixel = encoded_image.get_pixel(x, y);
                 let rgba = pixel.to_rgba();
-                let lsb = rgba.0[0] & 1;
-                current_byte = (current_byte << 1) | lsb;
-                bit_count += 1;
-                if bit_count == 8 {
-                    if current_byte == 0 {
-                        break; // End of message
+                for i in 0..3 {
+                    let channel_bit = rgba.0[i] & 1;
+                    current_byte = (current_byte << 1) | channel_bit;
+                    bit_count += 1;
+
+                    if bit_count == 8 {
+                        if current_byte == 0 {
+                            return String::from_utf8(message).expect("Invalid string");
+                        }
+                        message.push(current_byte);
+                        current_byte = 0u8;
+                        bit_count = 0;
                     }
-                    binary_message.push(current_byte as char);
-                    current_byte = 0u8;
-                    bit_count = 0;
                 }
             }
             if current_byte == 0 {
-                break; // End of message
+                return String::from_utf8(message).expect("Invalid string");
             }
         }
-        binary_message
+        String::from_utf8(message).expect("Invalid string")
     }
 
     pub fn decode_from_u8_array(input_image: &[u8]) -> Result<String, ImgSteganoError> {
